b9c5a551ef4d2aa79c6d4fd52f98c4e0cc34aa02
christianborn <chrisborn@hotmail.de>
2022-10-06 15:14:12 +0200

    Change feature branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Chris!");
         System.out.println("Hello new feature");
+        System.out.println("Hello new addition");
     }
 }
